f346822b13b68b804698509b372b0706218863f9
kolya04 <167567927+kolya04@users.noreply.github.com>
2024-11-10 20:45:14 +0500

    Add files via upload

---
+++ rimNumber.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+// Rim raqamlari qiymatlari
+int romanToInt(const string& s) {
+    unordered_map<char, int> romanValues = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    int result = 0;
+    int n = s.length();
+
+    for (int i = 0; i < n; ++i) {
+        int value = romanValues[s[i]];
+
+        // Keyingi belgi katta qiymatga ega bo'lsa, ayiramiz
+        if (i + 1 < n && value < romanValues[s[i + 1]]) {
+            result -= value;
+        }
+        else {
+            result += value;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    string roman;
+    cout << "Rim raqamini kiriting (masalan, XIV): ";
+    cin >> roman;
+
+    int arabic = romanToInt(roman);
+    cout << "Arab raqami: " << arabic << endl;
+
+    return 0;
+}
